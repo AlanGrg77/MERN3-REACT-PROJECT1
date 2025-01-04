90b1b14a8b6fc20e04ce0fbab0f81b164f2dd04d
AlanGrg77 <131133200+AlanGrg77@users.noreply.github.com>
2025-01-04 16:05:28 +0545

    Day 24 HomePage Integration

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Card from "../components/Card";
 import Navbar from "../components/Navbar";
 import axios from "axios";
 
 const HomePage = () => {
+  const [blogs,setBlogs] = useState([])
     const fetchBlogs = async () =>{
         const response = await axios.get('http://localhost:3000/blog')
+        setBlogs(response.data.data)
     } 
-
+    console.log(blogs)
     useEffect(()=>{
         fetchBlogs()
     },[])
@@ -15,15 +17,13 @@ const HomePage = () => {
     <>
       <Navbar />
       <div className="flex flex-wrap justify-center">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {
+          blogs.map((blog)=>{
+            return (
+              <Card blog={blog} />
+            )
+          })
+        } 
       </div>
     </>
   );
